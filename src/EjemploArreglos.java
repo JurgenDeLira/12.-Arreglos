36e7cceaa1c565db9b2236edcd974cd00206881b
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-06-10 11:55:56 -0700

    2

--- src/EjemploArreglos.java
+++ src/EjemploArreglos.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+
 public class EjemploArreglos {
     public static void main(String[] args) {
+
+        String[] productos = new String[7];
+        productos[0] = "Kingston Pendrive 64 GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco Duro SSD Samsung Externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "Macbook Air";
+        productos[5] = "Chromecast 4ta generación";
+        productos[6] = "Bicicleta Oxford";
+
+        Arrays.sort(productos);
+
+        String prod1 = productos[0];
+        String prod2 = productos[1];
+        String prod3 = productos[2];
+        String prod4 = productos[3];
+        String prod5 = productos[4];
+        String prod6 = productos[5];
+        String prod7 = productos[6];
+
+
+        System.out.println("productos[0] = " + prod1);
+        System.out.println("productos[1] = " + prod2);
+        System.out.println("productos[2] = " + prod3);
+        System.out.println("productos[3] = " + prod4);
+        System.out.println("productos[4] = " + prod5);
+        System.out.println("productos[5] = " + prod6);
+        System.out.println("productos[6] = " + prod7);
 
         int[] numeros = new int[4];
 
@@ -8,6 +38,8 @@ public class EjemploArreglos {
         numeros[2] = (int) 35L; // también, como si lo hicera entero
         numeros[3] = -1;
         //numeros[4] = 5;
+
+        Arrays.sort(numeros);
 
         int i = numeros[0];
         int j = numeros[1];
